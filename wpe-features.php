9b3b94206a642e9de917197e56850cca7a228874
Alan Blair <alan.blair@plana.com.au>
2019-12-30 10:07:08 +1000

    Updated WPE Library

--- wpe-features.php
+++ wpe-features.php
@@ -3,7 +3,7 @@
  * Plugin Name: WPEasy Features
  * Plugin URI:
  * Description: Common features for WP Easy clients
- * Version: 1.0.2
+ * Version: 1.0.3
  * Author: Alan Blair
  * Author URI:
  * Text Domain: wpeasy
